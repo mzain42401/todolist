6850d30dd746dea449753c3c58050ad36546e834
m zain ali <mzain42401@gmail.com>
2023-10-07 03:03:14 +0500

    second

--- src/Todo.jsx
+++ src/Todo.jsx
@@ -1,29 +1,91 @@
-import React from 'react'
+import React, { useRef, useState } from 'react'
 import {AiOutlinePlus,AiFillDelete,AiFillEdit,AiFillCheckCircle} from 'react-icons/ai'
+import {RiNewspaperFill} from "react-icons/ri"
 const Todo = () => {
+
+  const [userText,setUserText]=useState([])
+  const [isEdit,setIsEdit]=useState(false)
+  const [editData,setEditData]=useState()
+  const userRef=useRef()
+  const itemRef=useRef()
+  const editRef=useRef()
+
+
+  const addList=()=>{
+const text=userRef.current.value;
+setUserText([...userText,text])
+userRef.current.value=''
+setIsEdit(false)
+
+  }
+
+  const delAllList=()=>{
+
+    setUserText([])
+ setIsEdit(false)
+
+  }
+
+  const delItem=(elem)=>{
+const data=userText.filter((element)=>{
+  return(
+elem!==element
+  )
+})
+setUserText(data)
+  }
+
+const complete=(elem)=>{
+  itemRef.current.classList.toggle('active')
+}
+
+const editItem=(elem,index)=>{
+setIsEdit(true)
+const editValue=prompt('enter your value')
+setEditData(editValue)
+userText.splice(index,1,editData)
+ setUserText(userText)
+}
+
+
   return (
     <>
 
       <div className="container">
+        <h1> <RiNewspaperFill/>Todo List</h1>
         <div className="userInput">
-          <input id='todo' type="text" placeholder='add item' />
-          <div className='addListBtn'><AiOutlinePlus/></div>
-          <div className='addListBtn'><AiFillDelete/></div>
+          <input ref={userRef} id='todo' type="text" placeholder='add item' />
+          <div onClick={addList}  className='addListBtn'><AiOutlinePlus/></div>
+          <div onClick={delAllList} className='addListBtn'><AiFillDelete/></div>
 
         </div>
         <ul className='list'>
 
-          
-          <li>
+{
+  userText.map((elem,index)=>{
+return(
+<li>
             <div className='div1'>
-              <AiFillCheckCircle className='check'/>
-              <h3>zain</h3>
+              <AiFillCheckCircle onClick={complete} className='check'/>
+                <h3 ref={itemRef}>{elem}</h3>
+              
             </div>
             <div className='div2'>
-              <button><AiFillEdit/></button>
-              <button><AiFillDelete/></button>
+            <button onClick={()=>editItem(elem,index)}>
+                        <AiFillEdit/>
+              </button>
+              
+              
+              
+              <button onClick={()=>delItem(elem)}><AiFillDelete /></button>
             </div>
           </li>
+)
+  })
+}
+
+          
+         
          
 
         </ul>
