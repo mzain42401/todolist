45d3ca43ee757adc7c91a3744ddd4d49bc8b5727
m zain ali <mzain42401@gmail.com>
2023-10-07 13:36:19 +0500

    second

--- src/Todo.jsx
+++ src/Todo.jsx
@@ -1,92 +1,118 @@
 import React, { useRef, useState } from 'react'
-import {AiOutlinePlus,AiFillDelete,AiFillEdit,AiFillCheckCircle} from 'react-icons/ai'
-import {RiNewspaperFill} from "react-icons/ri"
+import { AiOutlinePlus, AiFillDelete, AiFillEdit, AiFillCheckCircle } from 'react-icons/ai'
+import { RiNewspaperFill } from "react-icons/ri"
 const Todo = () => {
 
-  const [userText,setUserText]=useState([])
-  const [isEdit,setIsEdit]=useState(false)
-  const [editData,setEditData]=useState()
-  const userRef=useRef()
-  const itemRef=useRef()
-  const editRef=useRef()
+  const [userText, setUserText] = useState([])
+  const [inputData,setInputData]=useState()
+  const [toggle, settoggle] = useState(false)
+  const [id, setId] = useState(null)
+  const itemRef = useRef()
 
 
-  const addList=()=>{
-const text=userRef.current.value;
-setUserText([...userText,text])
-userRef.current.value=''
-setIsEdit(false)
+  const addList = () => {
+
+    if (inputData.length > 0 && !toggle) {
+      const createId = new Date().getTime().toString()
+      const Data = { id: createId, task: inputData ,complete:false};
+
+      setUserText([...userText, Data])
+      setInputData('')
+    }
+    else if (inputData.length > 0 && toggle) {
+
+
+      setUserText(userText.map((elem) => {
+        if (elem.id === id) {
+          return { ...elem, task: inputData }
+        }
+        return elem
+      }))
+      settoggle(false)
+      setInputData('')
+    }
+
+    else {
+      alert("enter value")
+    }
+    // setIsEdit(false)
+
 
   }
 
-  const delAllList=()=>{
+  const delAllList = () => {
 
     setUserText([])
- setIsEdit(false)
 
   }
 
-  const delItem=(elem)=>{
-const data=userText.filter((element)=>{
-  return(
-elem!==element
-  )
-})
-setUserText(data)
+  const delItem = (elem) => {
+    const data = userText.filter((element) => {
+      return (
+        elem !== element
+      )
+    })
+    setUserText(data)
   }
 
-const complete=(elem)=>{
-  itemRef.current.classList.toggle('active')
-}
+  const complete = (id) => {
+    const filter=userText.filter((elem)=>{
 
-const editItem=(elem,index)=>{
-setIsEdit(true)
-const editValue=prompt('enter your value')
-setEditData(editValue)
-userText.splice(index,1,editData)
- setUserText(userText)
-}
+  return elem.id===id
 
+    })
+    filter[0].complete=true
 
+    console.log(filter);
+
+    // filter.complete=true
+    // itemRef.current.classList.toggle('active')
+  }
+
+  const editItem = (elem, id) => {
+    settoggle(true)
+    setInputData(elem.task)
+    setId(id)
+
+  }
   return (
     <>
-
       <div className="container">
-        <h1> <RiNewspaperFill/>Todo List</h1>
+        <h1> <RiNewspaperFill />Todo List</h1>
         <div className="userInput">
-          <input ref={userRef} id='todo' type="text" placeholder='add item' />
-          <div onClick={addList}  className='addListBtn'><AiOutlinePlus/></div>
-          <div onClick={delAllList} className='addListBtn'><AiFillDelete/></div>
-
+          <input className={` ${toggle ? "lightgreen" : "whitebbackroung"}`}
+          value={inputData} onChange={(e) => setInputData(e.target.value)} id='todo' type="text" placeholder='add item' />
+          {
+            toggle ?
+              <div className={`addListBtn ${toggle ? "lightgreen" : null}`}  onClick={addList}>
+                <AiFillEdit /></div> :
+              <div className='addListBtn' onClick={addList} > <AiOutlinePlus /></div>
+          }
+          <div onClick={delAllList} className='addListBtn'><AiFillDelete /></div>
         </div>
         <ul className='list'>
+          {
+            userText.map((elem) => {
+              return (
+                <li key={elem.id}>
+                  <div className='div1'>
+                    <AiFillCheckCircle onClick={()=>complete(elem.id)} className='check' />
+                    <h3 ref={itemRef}>{elem.task}</h3>
+                  </div>
+                  <div className='div2'>
+                    <button onClick={() => editItem(elem, elem.id)}>
+                      <AiFillEdit />
+                    </button>
+                    <button onClick={() => delItem(elem)}><AiFillDelete /></button>
+                  </div>
+                </li>
+              )
+            })
+          }
 
-{
-  userText.map((elem,index)=>{
-return(
-<li>
-            <div className='div1'>
-              <AiFillCheckCircle onClick={complete} className='check'/>
-                <h3 ref={itemRef}>{elem}</h3>
-              
-            </div>
-            <div className='div2'>
-            <button onClick={()=>editItem(elem,index)}>
-                        <AiFillEdit/>
-              </button>
-              
-              
-              
-              <button onClick={()=>delItem(elem)}><AiFillDelete /></button>
-            </div>
-          </li>
-)
-  })
-}
 
-          
-         
-         
+
+
 
         </ul>
 
